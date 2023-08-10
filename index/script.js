436d1d9f509043a558224dd5d06e627738bae5ab
arpit <arpit2003y@gmail.com>
2023-08-10 21:21:12 +0530

    added floating search bar in small device veiw

--- index/script.js
+++ index/script.js
@@ -1,4 +1,6 @@
 const searchInput = document.getElementById("search");
+const searchMobile = document.querySelector(".floating-search-div");
+const floatingInputBar = document.getElementById("floating-input-bar");
 
 searchInput.addEventListener("click", () => {
     searchInput.placeholder = ""
@@ -6,5 +8,27 @@ searchInput.addEventListener("click", () => {
 
 searchInput.addEventListener("blur", () => {
     searchInput.placeholder = "Search for products brands and more "
+})
+
+
+function showFloatingSearch() {
+    if (searchMobile.style.display == "block") {
+        searchMobile.style.display = "none";
+    }
+    else {
+        searchMobile.style.display = "block";
+    }
+}
+
+function closeFloatingSearch() {
+    searchMobile.style.display = "none";
+}
+
+floatingInputBar.addEventListener("click", () => {
+    floatingInputBar.placeholder = " ";
+})
+
+floatingInputBar.addEventListener("blur", () => {
+    floatingInputBar.placeholder = "Search for products";
 })
 
