ab06e40eab5fc710f2461d0d02eba929d750bfdf
arpit <arpit2003y@gmail.com>
2023-09-02 16:17:26 +0530

    changed login pages

--- index/script.js
+++ index/script.js
@@ -7,7 +7,7 @@ const loader = document.getElementById("preloader");
 window.addEventListener("load", () => {
     setTimeout(() => {
         loader.style.display = "none";
-    }, 2000)
+    }, 1000)
 
 });
 
